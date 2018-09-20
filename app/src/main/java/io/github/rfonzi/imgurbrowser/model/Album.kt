877d1aa5f0ea4304a9c184c097d8a759062eeb98
Ryan Fonzi <ryan.fonzi@gmail.com>
2018-09-20 17:21:50 -0400

    Gallery: Add info get for upvotes

--- app/src/main/java/io/github/rfonzi/imgurbrowser/model/Album.kt
+++ app/src/main/java/io/github/rfonzi/imgurbrowser/model/Album.kt
@@ -4,6 +4,9 @@ package io.github.rfonzi.imgurbrowser.model
  * Created by Ryan on 2/27/2018.
  */
 data class Album(
+        val title: String,
+        val submitter: String,
         val albumLink: String,
-        val images: List<Image>
+        val images: List<Image>,
+        val upvotes: Int
 )
